b3d149ec5e71850d952d3df565a1f57830d58168
zhenyu <khoozhenyu@outlook.com>
2022-04-29 11:17:26 +0900

    wanna kashoot myself

---
+++ Week 4/test.py
@@ -0,0 +1,26 @@
+# number = 4
+# if number < 10:
+#     print(number, 'is less than 10')
+
+# if number < 10 and number*2 > 7:
+#     print(number, 'is less than 10 and and multiplying it by 2 results in more than 7')
+
+# number = 10
+
+# if number > 100:
+#     print('number is greater than 100')
+# elif number == 50:
+#     print('number is 50')
+# elif number < 20:
+#     print('number is less than 20')
+# else:
+#     print('all above evaluated False')
+
+number = 17
+if number >= 0:
+    if number == 0:
+        print('zero')
+    else:
+        print('positive')
+else:
+    print('negative')
