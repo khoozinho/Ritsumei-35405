a6c51d8580c072103d1ea86e99f5df5fb4388867
zhenyu <khoozhenyu@outlook.com>
2022-05-20 11:32:37 +0900

    holy shit this exercise is so long

---
+++ Week 7/handout7.py
@@ -0,0 +1,71 @@
+# lang_tuple = ("FORTRAN", "COBOL", "LISP")
+# print("initial tuple:", lang_tuple, "\ndata type:", type(lang_tuple))
+# print("second element:", lang_tuple[1])
+
+# my_tuple = (10, 20, 30, 40)
+
+# n1, n2, n3, n4 = my_tuple
+# print("For example, number 2:", n2)
+
+# sd = {
+#     'Da Vinci': 1452, 
+#     'Galilei': 1564, 
+#     'Newton': 1642
+# }
+
+# elements = list(sd.items())
+# print("List of dict items in a list of tuples:", elements)
+# print("First element:", elements[0])
+
+# my_set = {2, 3, 4, "apples"}
+# print("My set:", my_set)
+
+# lang_set = {"FORTRAN", "COBOL", "LISP"}
+# print("Original language set:", my_set)
+# lang_set.add("C++")
+# print("New language set:", lang_set)
+
+# lang_set.add("FORTRAN")
+# print("Language set was not modified:", lang_set)
+
+# lang_set = {"FORTRAN", "COBOL", "LISP"}
+# print("Original language set:", lang_set)
+# lang_set.remove("COBOL")
+# print("Modified language set:", lang_set)
+# lang_set.pop()
+# print("Modified language set:", lang_set)
+
+# ls1 = {"FORTRAN", "COBOL", "LISP"}
+# ls2 = {"C++", "COBOL"}
+
+# print("Original language sets:", ls1, ",", ls2)
+
+# inter = ls1.intersection(ls2)
+# diff = ls1.difference(ls2)
+
+# print("Elements that exist in both ls1 and ls2:", inter)
+# print("Elements that exist in ls1 but not in ls2:", diff)
+
+# ls1.update(ls2)
+# print("Updated ls1:", ls1)
+
+# my_list = ["FORTRAN", "COBOL", "FORTRAN", 1, 2, 2]
+# my_set = set(my_list)
+
+# print("Original list:", my_list, '\ntype:', type(my_list))
+# print("duplicates removed:", my_set, '\ntype:', type(my_set))
+
+# my_list = list(set(my_list))
+# print("The list in dupes removed:", my_list, '\ntype:', type(my_list))
+
+my_list = ["FORTRAN", "COBOL", "FORTRAN", 1, 2, 2]
+my_list = list(dict.fromkeys(my_list))
+print("Duplicates removed and order preserved:", my_list)
+
+my_tuple = ("apple", "apple", "orange")
+my_set = {"apple", "apple", "orange"}
+
+print("My tuple:", my_tuple, '\nMy set:', my_set)
+print('The word (string) "orange" is an element in the tuple:', "orange" in my_tuple)
+print('The word (string) "lemon" is not an element in the set:', "lemon" not in my_tuple)
+
