bcdcabc61464f976de3b2a9823a076eb415c34dd
zhenyu <khoozhenyu@outlook.com>
2022-06-11 15:56:19 +0900

    what the heck is numpy?

--- Week 10/work.py
+++ Week 10/work.py
@@ -7,7 +7,11 @@ def make_array(filename):
     """
     with open(filename, 'r', encoding='utf-8') as f:
         textarray = f.read().splitlines()
-    return np.array(textarray)
+        lst = []
+        for i in textarray:
+            floats = float(i)
+            lst.append(floats)
+        return np.array(lst)
 
 read_arr = make_array("numbers.txt")
 print("Array created from the numbers of the text file:", read_arr)
@@ -18,7 +22,7 @@ arr_1 = np.linspace(5, 100, num = 30)
 print("\nArray created with 30 values spaced linearly from 5 to 100:", arr_1)
 
 # arr_1 is redefinied as arr_1 + read_arr
-arr_1 = np.concatenate((arr_1, read_arr))
+arr_1 = arr_1 + read_arr
 
 # Create a 1D number random array with 30 floating point numbers
 arr_2 = random.rand(30)
@@ -28,3 +32,10 @@ print("\narr2:" , arr_2)
 
 # join the arrays arr_1 and arr_2 along axis 0 using np.stack()
 stacked_array = np.stack((arr_1, arr_2), axis=0)
+print("\nStacked array:", stacked_array)
+
+average = np.mean(stacked_array[0], axis=0)
+print("\nAverage of the stacked array[0]:", average)
+
+summa = np.sum(stacked_array[1], axis=0)
+print("\nSum of the stacked array[0]:", summa)
