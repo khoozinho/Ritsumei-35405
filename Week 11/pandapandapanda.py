29a3712633ec58dd4adc6e375d4bf6094bb0f8cc
zhenyu <khoozhenyu@outlook.com>
2022-06-17 13:21:54 +0900

    fuckkkkkkkking stressssss

--- Week 11/pandapandapanda.py
+++ Week 11/pandapandapanda.py
@@ -9,3 +9,8 @@ titanic = pd.read_excel("titanic.xlsx", sheet_name="passengers")
 
 ages = titanic["Age"]
 print(ages.head())
+print(ages.shape)
+
+above_35 = titanic[titanic["Age"] > 35]
+print(above_35.head())
+print(above_35.shape)
