a81e93bbeca46b4d25ecddaaf794ce478e24ab63
zhenyu <khoozhenyu@outlook.com>
2022-07-08 11:50:01 +0900

    kachow

--- Week 14/car.py
+++ Week 14/car.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Author: Khoo Zhenyu
-Student ID: 2600220458-3
-Program description:
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# Author: Khoo Zhenyu
+# Student ID: 2600220458-3
+# Program description:
 
-"""
+# """
 
 class Car:
     classification = 'vehicle'
